b5639f1009db0bb3d7e0ebc9a7d67886a18aa6b3
adasko90 <adasko90@gmail.com>
2014-07-17 20:39:02 +0200

    Trying to make tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :trackable, :validatable
 
+    validates :email, presence: true
+	validates :encrypted_password, presence: true
+
     has_many :comments
 	has_many :posts
 	
