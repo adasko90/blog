76810260d1e155813c955b41d9197aa4c5e33f8b
adasko90 <adasko90@gmail.com>
2014-08-13 22:23:06 +0200

    Added nickname and admin fields to users

--- db/migrate/20140813201942_add_fields_to_user.rb
+++ db/migrate/20140813201942_add_fields_to_user.rb
@@ -1,4 +1,6 @@
 class AddFieldsToUser < ActiveRecord::Migration
   def change
+        add_column :users, :nickname, :string
+        add_column :users, :admin, :boolean, :default => false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140717093335) do
+ActiveRecord::Schema.define(version: 20140813201942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,15 +29,17 @@ ActiveRecord::Schema.define(version: 20140717093335) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",              default: "", null: false
-    t.string   "encrypted_password", default: "", null: false
-    t.integer  "sign_in_count",      default: 0,  null: false
+    t.string   "email",              default: "",    null: false
+    t.string   "encrypted_password", default: "",    null: false
+    t.integer  "sign_in_count",      default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "nickname"
+    t.boolean  "admin",              default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
