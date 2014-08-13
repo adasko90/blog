52864b64711743fcf3ab6005324475c876511df9
adasko90 <adasko90@gmail.com>
2014-08-13 22:36:52 +0200

    Added some validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :trackable, :validatable
 
-  validates :email, presence: true
-	validates :encrypted_password, presence: true
+  validates :email, :encrypted_password, :nickname, presence: true
+
 
   has_many :comments
 	has_many :posts
